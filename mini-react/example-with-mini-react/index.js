9c3293d0f95faf0e66c299620af682e006412c2c
山丘 <gengze@innotechx.com>
2019-12-03 00:31:27 +0800

    feat: 添加MiniReact.createElement方法

--- mini-react/example-with-mini-react/index.js
+++ mini-react/example-with-mini-react/index.js
@@ -1,76 +1,47 @@
+/** @jsx MiniReact.createElement */
+import MiniReact from "../src/mini-react";
 import MiniReactDOM from "../src/mini-react-dom";
+import cn from "classnames";
 import "./index.pcss"
 
-MiniReactDOM.render({
-  type: "section",
-  props: {
-    children: [{
-      type: "h1",
-      props: {
-        children: "TODOS"
-      },
-    }, {
-      type: "ul",
-      props: {
-        children: [{
-          type: "li",
-          props: {
-            className: "item item--done",
-            children: [{
-              type: "input",
-              props: {
-                id: "1",
-                type: "checkbox",
-                checked: true,
-              }
-            }, {
-              type: "label",
-              props: {
-                for: "1",
-                children: "起床",
-              }
-            }]
-          }
-        }, {
-          type: "li",
-          props: {
-            className: "item",
-            children: [{
-              type: "input",
-              props: {
-                id: "2",
-                type: "checkbox",
-                checked: false,
-              }
-            }, {
-              type: "label",
-              props: {
-                for: "2",
-                children: "洗漱",
-              }
-            }]
-          }
-        }, {
-          type: "li",
-          props: {
-            className: "item",
-            children: [{
-              type: "input",
-              props: {
-                id: "3",
-                type: "checkbox",
-                checked: false,
-              }
-            }, {
-              type: "label",
-              props: {
-                for: "3",
-                children: "打酱油",
-              }
-            }]
-          }
-        }]
-      }
-    }]
-  },
-}, document.getElementById("root"));
+const todos = [{
+	id: 1,
+	txt: "起床",
+	done: true,
+}, {
+	id: 2,
+	txt: "洗漱",
+	done: false,
+}, {
+	id: 3,
+	txt: "打酱油",
+	done: false,
+}]
+const renderTodoItem = (item, index) => {
+  return (
+    <li className={cn("item", {"item--done": item.done})} onClick={(e) => {
+      e.preventDefault();
+      toggleTodo(item.id);
+    }}>
+      <input id={index} type="checkbox" checked={item.done} />
+      <label htmlFor={index}>{item.txt}</label>
+    </li>
+  )
+}
+const getApp = (todos) => {
+  return (
+    <section>
+      <h1>TODOS</h1>
+      <ul>
+        {todos.map(renderTodoItem)}
+      </ul>
+    </section>
+  )
+}
+const toggleTodo = (id) => {
+  const item = todos.filter(todo => todo.id === id)[0]
+  item.done = !item.done;
+  MiniReactDOM.render(getApp(todos), document.getElementById("root"));
+}
+
+MiniReactDOM.render(getApp(todos), document.getElementById("root"));
--- mini-react/src/mini-react.js
+++ mini-react/src/mini-react.js
@@ -1,5 +1,24 @@
-const createElement = (...args) => {
-  console.log(args);
+const flattenArray = (arr) => {
+  if (!arr && arr.length <= 0) {
+    return [];
+  }
+  return arr.reduce((acc, item) => {
+    if (Object.prototype.toString.call(item) === "[object Array]") {
+      acc = [...acc, ...flattenArray(item)];
+    } else {
+      acc.push(item);
+    } 
+    return acc;
+  }, []);
+}
+const createElement = (type, config, ...args) => {
+  const props = Object.assign({}, config, {
+    children: flattenArray(args),
+  });
+  return {
+    type,
+    props,
+  }
 }
 
 export default {
